8af9e3105671d2e959d9171edc7e9d3dabef7bc9
Ihor Mohyla <l.ihor.p.mogyla@gmail.com>
2014-09-18 09:55:56 +0300

    first deploy  Please enter the commit message for your changes. Lines starting

---
+++ config/default.js
@@ -0,0 +1,9 @@
+module.exports = {
+    memcached : {
+        host: '127.0.0.1',
+        port: '11211',
+        options: {
+            poolSize: 32
+        }
+    }
+};
---
+++ index.js
@@ -0,0 +1,18 @@
+var memcached = require('./lib/co-memcached');
+var co = require('co');
+
+
+co(function* () {
+    var key = 'test';
+    yield memcached.set(key, 7, 3600);
+    var get  = yield memcached.get(key);
+    console.log('get--->>>', get);
+
+    var del  = yield memcached.delete(key);
+
+    var get  = yield memcached.get(key);
+    console.log('get--->>>', get);
+
+})();
+
+
